2f16f2de4dcd750ce47a6eec941c3659dcda97f8
pirooz <pirooz@gmail.com>
2013-10-10 15:55:02 -0700

    fixing some bugs in hdr2nii.py

--- hdr2nii.py
+++ hdr2nii.py
@@ -1,7 +1,11 @@
+from nipype import config, logging
 import sys, getopt, glob
 import nipype.pipeline.engine as pe
 import nipype.interfaces.fsl as fsl
 from nipype.interfaces.utility import Function
+
+#config.enable_debug_mode()
+#logging.update_logging(config)
 
 def parse_args(argv):
   global input_dir, output_dir
@@ -20,54 +24,56 @@ def parse_args(argv):
       input_dir = arg
     elif opt in ("-o", "--odir"):
       output_dir = arg
-  print 'Input file is "', input_dir
-  print 'Output file is "', output_dir
+  print 'Input directory is "', input_dir
+  print 'Output directory is "', output_dir
 
 
 parse_args(sys.argv[1:])
 
 #change the type of .hdr to .nii
-changeDTer = pe.Node(interface = fsl.ChangeDataType(), name = 'changeDT')
-changeDTer.base_dir = input_dir
-changeDTer.iterables = ("in_file", [f for f in glob.glob(input_dir + '*.hdr')])
-changeDTer.inputs.output_type = 'NIFTI_GZ'
-changeDTer.inputs.output_datatype = 'input'
-changeDTer.inputs.terminal_output = 'none'
+changeDT = pe.Node(interface=fsl.ChangeDataType(), name='changeDT')
+changeDT.base_dir = input_dir
+changeDT.iterables = ("in_file", [f for f in glob.glob(input_dir + '*.hdr')])
+changeDT.inputs.output_type = 'NIFTI_GZ'
+changeDT.inputs.output_datatype = 'input'
+changeDT.inputs.terminal_output = 'none'
 
 #function to interface with fslorient command
-def fslorient(in_file_name, *main_option):
+def fslorient(in_file_name, main_options):
   import subprocess
-  args = ["fslorient"]
-  for each in main_option:
-    args.append(each)
-  args.append(in_file_name)
-  return subprocess.call(args)
+  command = ["fslorient"]
+  for option in main_options:
+    command.append(option)
+  command.append(in_file_name)
+  subprocess.call(command)
+  return in_file_name
 
 
-fslorient_interface = Function(input_names = ["in_file_name", "*main_option"],
-                               output_names = ["out_file_name"],
-                               function = fslorient)
+fslorient_interface = Function(input_names=["in_file_name", "main_options"],
+                               output_names=["out_file_name"],
+                               function=fslorient)
 
 #delete the orientation of .nii image
-deleteOrienter = pe.Node(interface = fslorient_interface, name = 'deleteOrient')
-deleteOrienter.inputs.main_option = '-deleteorient'
+deleteOrient = pe.Node(interface=fslorient_interface, name='deleteOrient')
+deleteOrient.base_dir = input_dir
+deleteOrient.inputs.main_options = '-deleteorient'
 
 #Swap dimension, fixes the dimension of the .nii file to x, y, z coordinates
-swapDimer = pe.Node(interface = fsl.SwapDimensions(), name = 'swapDimension')
-swapDimer.inputs.new_dims = 'x', 'y', 'z'
-swapDimer.inputs.terminal_output = 'file'
+swapDim = pe.Node(interface=fsl.SwapDimensions(), name='swapDimension')
+swapDim.inputs.new_dims = 'x', 'y', 'z'
+swapDim.inputs.terminal_output = 'file'
 
 #calls fslorient interface and setsqformcode.
-setqfc = pe.Node(interface = fslorient_interface, name = 'setqfc')
-setqfc.inputs.main_option ='-setqformcode', '1'
+setqfc = pe.Node(interface=fslorient_interface, name='setqfc')
+setqfc.inputs.main_options = '-setqformcode', '1'
 
 #create the workflow, add nodes, connect them and run the workflow
 workflow = pe.Workflow(name='hdr2nii')
 workflow.base_dir = output_dir
-workflow.add_nodes([changeDTer, deleteOrienter, swapDimer, setqfc])
+workflow.add_nodes([changeDT, deleteOrient, swapDim, setqfc])
 
-workflow.connect(changeDTer, 'out_file', deleteOrienter, 'in_file_name')
-workflow.connect(changeDTer, 'out_file', swapDimer, 'in_file')
-workflow.connect(changeDTer, 'out_file', setqfc, 'in_file_name')
+workflow.connect(changeDT, 'out_file', deleteOrient, 'in_file_name')
+workflow.connect(changeDT, 'out_file', swapDim, 'in_file')
+workflow.connect(changeDT, 'out_file', setqfc, 'in_file_name')
 
 workflow.run()
